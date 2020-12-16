fd6c8c61da2df96c30224395a0fad04d64c08b74
marcos <marcos.randel@agna.com.br>
2020-12-16 08:18:39 -0300

    First commit

---
+++ android/app/src/main/kotlin/com/example/shopnet/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.shopnet
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/utils/constants.dart
@@ -0,0 +1,3 @@
+class Constants {
+  static const BASE_API_URL = 'https://shop-8439b-default-rtdb.firebaseio.com/';
+}
